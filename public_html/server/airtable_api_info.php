3608a2b73b17da929bff231dc56e23c594f6745c
Tristan Lopus <tristan.lopus@gmail.com>
2020-06-23 09:23:32 -0700

    removed sensitive files

--- public_html/server/airtable_api_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  $airtable_api_key = "keydn7CwS79jE483I";
-
-  $airtable_base_id = "appdtuSkkUVz2W7fs"; //appYMXlYUdAWai01r
-
-?>
--- public_html/server/webmaster_password.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $webmasterPassword = "repair2016MapRoom";
-?>
